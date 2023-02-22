da33f2916dd6862af89161e35199e6aff17c5202
Ankit Patel <ankit.patel.3794@gmail.com>
2023-02-22 11:20:47 +0530

    updated to create_app function

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-from flask import Flask
-from core.apis import api
-from core import db
-from core.models.todo import ToDoModel
+from core import create_app
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
-app.config['SECRET_KEY'] = "1234532"
-
-api.init_app(app)
-db.init_app(app)
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
